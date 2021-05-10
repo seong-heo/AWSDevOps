82223c0cf24efa407c25cf14486591f0b12c98ea
seongheon <seongheon@test.com>
2021-05-11 01:14:39 +0900

    Test php

---
+++ test1.php
@@ -0,0 +1,23 @@
+<?php
+echo '<style>
+table{
+border: 1px solid salmon;
+}
+tr,td{
+border: 1px solid deepskyblue;
+}
+</style>
+<head><title>Hello PHP</title></head>
+<body>
+<b>hi</b>
+<table>
+<tr>
+    <td>name</td>
+    <td>seongheon</td>
+</tr>
+<tr>
+    <td>age</td>
+    <td>26</td>
+</tr>
+</table>
+</body>';
